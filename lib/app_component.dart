a16d77b3ee100eb1b08950ff8ab604c73a8402ad
ofadeyi <ofadeyi@gmail.com>
2016-07-25 01:08:30 +0100

    First Angular Dart tutorial

---
+++ lib/app_component.dart
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, ofadeyi. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+
+@Component(
+    selector: 'my-app',
+    templateUrl: 'app_component.html')
+class AppComponent {}
